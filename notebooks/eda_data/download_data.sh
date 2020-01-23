c8cf5b5f27634ebfa61a1c6d8943f88c7ec9906c
leandro <leandro@ebi.ac.uk>
2020-01-23 20:18:40 +0000

    Updating EDA

---
+++ notebooks/eda_data/download_data.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -eux
+rsync -avR noah:/hps/nobackup/research/zi/leandro/snippy_calls_gene_distance/evaluating_calls_*/get_gene_truth_ref_*_proportion_distance/all_gene_truth_ref_*_proportion_distance.csv .
