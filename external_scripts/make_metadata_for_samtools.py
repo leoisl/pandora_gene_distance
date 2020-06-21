0dc79d6e0f697dbf07a6b760675205e2241e078d
leandro <leandro@ebi.ac.uk>
2020-06-21 18:08:14 +0100

    rolling back updates to make_metadata_for_samtools.py

--- external_scripts/make_metadata_for_samtools.py
+++ external_scripts/make_metadata_for_samtools.py
@@ -1,7 +1,17 @@
+base_path="/hps/nobackup/iqbal/leandro/samtools_analysis_pipeline/illumina/100x/random/063_STEC"
+
+from glob import glob
 import pandas as pd
-df = pd.read_csv("/hps/nobackup/iqbal/leandro/pandora1_paper/metadata/variant_calls.20_way.pandora.snippy.samtools.illumina.100x.csv")
-samtools_df = df[df.tool.apply(lambda tool: tool.startswith("samtools"))]
-samtools_df = samtools_df[samtools_df.sample_id == "063_STEC"]
-samtools_df["id"] = samtools_df["tool"].apply(lambda tool : tool.replace("samtools_", ""))
-samtools_df["fasta"] = samtools_df["reference"]
-samtools_df[["id", "fasta"]].to_csv("references_samtools.csv", index=False)
+from pathlib import Path
+
+ids=[]
+fastas=[]
+
+for file in glob(f"{base_path}/*.ref.fa"):
+    id = Path(file).name
+    id = id.replace("samtools_063_STEC_AND_", "").replace(".ref.fa", "")
+    ids.append(id)
+    fastas.append(str(file))
+
+df = pd.DataFrame({"id": ids, "fasta": fastas})
+df.to_csv("references_samtools.csv", index=False)
