ef9105261315bb1e303086e103b729b61fbd62ba
leandro <leandro@ebi.ac.uk>
2020-09-13 07:42:01 +0100

    Updating notebooks

--- notebooks/eda_data/download_data.sh
+++ notebooks/eda_data/download_data.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -eux
-rsync -avR noah:/hps/nobackup/research/zi/leandro/snippy_calls_gene_distance/evaluating_calls_*/get_gene_truth_ref_*_proportion_distance/all_gene_truth_ref_*_proportion_distance.csv .
+rsync -avR yoda-ext:/hps/nobackup/iqbal/leandro/snippy_calls_gene_distance/gene_distance_plots_20_way_snippy_1_random_reference_illumina_100x/get_gene_truth_ref_*_proportion_distance/all_gene_truth_ref_*_proportion_distance.csv .
